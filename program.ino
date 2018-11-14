36295661ac223997d338bed379878cd012f70ff2
Michael Schindler <mi-schi@users.noreply.github.com>
2018-11-14 19:07:55 +0100

    Optimize programs for attiny24

--- program.ino
+++ program.ino
@@ -1,242 +1,123 @@
-int lightPins[] = {3, 4, 5, 6, 7, 8, 9, 10};
-int programPin = 2;
-int speedPin = A0;
+byte lightPins[8] = {5, 4, 3, 2, 1, 0, 9, 10};
 
-int program = 0;
-int msMinPerProgram[] = {  5,  50,  50,  60,  50,  50,  40,  30,  15,  15,  40,  40,  30,  30,   5};
-int msMaxPerProgram[] = {100, 300, 400, 600, 500, 400, 500, 300, 400, 400, 400, 400, 400, 400, 100};
-unsigned long programSwitchTime = 0;
-int programSwitchDelay = 500;
+byte allLights[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+byte halfLights[2][4] = {
+  {0, 1, 2, 3},
+  {4, 5, 6, 7}
+};
+byte splittedBounce[6] = {0, 1, 2, 3, 2, 1};
+byte splittedLights[4][4][2] = {
+  {{3, 4}, {2, 5}, {1, 6}, {0, 7}},
+  {{3, 7}, {2, 6}, {1, 5}, {0, 4}},
+  {{0, 7}, {1, 6}, {2, 5}, {3, 4}},
+  {{0, 4}, {1, 5}, {2, 6}, {3, 7}}
+};
+
+volatile byte program = 0;
+volatile bool programSwitched = false;
+volatile unsigned long programSwitchTime = 0;
 
 void setup() {
-  // Serial.begin(9600);
-  // Serial.println();
-  for (int i=0; i<8; i++) {
+  for (byte i = 0; i < 8; i++) {
     pinMode(lightPins[i], OUTPUT);
   }  
-  pinMode(programPin, INPUT);
-  switchLights("01234567", HIGH);
-  delay(1000);
-  switchLights("01234567", LOW);
-  delay(300);
+  pinMode(8, INPUT);
+  attachInterrupt(0, programSwitch, RISING);
+
+  flasher(20, 6);
 }
 
 void loop() {
-  if (program == 1) {
-    // police 2 times
-    String sequences[] = {"0123", "4567"};
-    for (int i=0; i<2; i++) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
-      
-      if (sensoredDelay(1.5)) return;
-    }
-  } else if (program == 2) {
-    // out to in splitted
-    String sequences[] = {"07", "16", "25", "34"};
-    for (int i=0; i<3; i++) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
-    } 
-    for (int i=3; i>0; i--) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
+  if (programSwitched) {
+    switchLights(allLights, 8, 0);
+    programSwitched = false;
+  }
+  if (program == 1 || program == 2) {
+    // flasher / flasher 2 times
+    flasher(6, (program == 2) ? 3 : 6);
+    if (program == 2) {
+      flasher(6, 12);
     }
   } else if (program == 3) {
-    // flasher
-    switchLights("01234567", HIGH);
-    if (sensoredDelay(1)) return;
-    switchLights("01234567", LOW);
-    if (sensoredDelay(1)) return;
-  } else if (program == 4) {
-    // left to right splitted
-    String sequences[] = {"04", "15", "26", "37"};
-    for (int i=0; i<3; i++) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
-    } 
-    for (int i=3; i>0; i--) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-      switchLights(sequences[i], LOW);
-    }
-  } else if (program == 5) {
-    // in to out splitted
-    String sequences[] = {"34", "25", "16", "07"};
-    for (int i=0; i<4; i++) {  
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-    }
-    switchLights("01234567", LOW);
-    if (sensoredDelay(2)) return;
-  } else if (program == 6) {
     // police
-    switchLights("4567", LOW);
-    switchLights("0123", HIGH);
-    if (sensoredDelay(2)) return;
-    switchLights("0123", LOW);
-    switchLights("4567", HIGH);
-    if (sensoredDelay(2)) return;
-  } else if (program == 7) {
-    // flasher 2 times
-    switchLights("01234567", HIGH);
-    if (sensoredDelay(2)) return;
-    switchLights("01234567", LOW);
-    if (sensoredDelay(1)) return;
-    switchLights("01234567", HIGH);
-    if (sensoredDelay(2)) return;
-    switchLights("01234567", LOW);
-    if (sensoredDelay(4)) return;  
-  } else if (program == 8) {
-    // snake
-    for (int i=0; i<=7; i++) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
+    switchLights(halfLights[0], 4, 1);
+    switchLights(halfLights[1], 4, 0);
+    sensoredDelay(8);
+    switchLights(halfLights[0], 4, 0);
+    switchLights(halfLights[1], 4, 1);
+    sensoredDelay(8);
+  } else if (program == 4) {
+    // police 2 times
+    for (byte l = 0; l < 2; l++) {
+      for (byte i = 1; i < 5; i++) {
+        switchLights(halfLights[l], 4, i % 2);
+        sensoredDelay(4);
+      }
+      sensoredDelay(2);
     }
-    for (int i=0; i<=7; i++) {
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(1)) return;
+  } else if (program == 5 || program == 6) {
+    // splitted one out to in bounce / splitted one left to right bounce 
+    for (byte i = 0; i < 6; i++) {
+      switchLights(splittedLights[program - 5][splittedBounce[i]], 2, 1);
+      sensoredDelay(4);
+      switchLights(splittedLights[program - 5][splittedBounce[i]], 2, 0);
     }
-  } else if (program == 9) {
-    // snake reverse
-    for (int i=7; i>=0; i--) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
+  } else if (program >= 7 && program <= 10) {
+    // splitted out to in snake / splitted left to right snake
+    // splitted in to out snake / splitted right to left snake
+    for (byte value = 0; value < 2; value++) {
+      for (byte i = 0; i < 4; i++) {
+        switchLights(splittedLights[program - 7][i], 2, value);
+        sensoredDelay(4);
+      }
     }
-    for (int i=7; i>=0; i--) {
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(1)) return;
-    }
-  } else if (program == 10) {
-    // out to in full
-    String sequences[] = {"07", "16", "25", "34"};
-    for (int i=0; i<4; i++) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-    }
-    for (int i=0; i<4; i++) {
-      switchLights(sequences[i], LOW);
-      if (sensoredDelay(1)) return;
-    }
-  } else if (program == 11) {
-    // in to out full
-    String sequences[] = {"34", "25", "16", "07"};
-    for (int i=0; i<4; i++) {
-      switchLights(sequences[i], HIGH);
-      if (sensoredDelay(1)) return;
-    }
-    for (int i=0; i<4; i++) {
-      switchLights(sequences[i], LOW);
-      if (sensoredDelay(1)) return;
-    }
-  } else if (program == 12) {
-    // left to right two lights
-    digitalWrite(lightPins[0], HIGH);
-    if (sensoredDelay(1)) return;
-    digitalWrite(lightPins[1], HIGH);
-    if (sensoredDelay(1)) return;
-    for (int i=1; i<7; i++) {
-      digitalWrite(lightPins[i-1], LOW);
-      digitalWrite(lightPins[i], HIGH);
-      digitalWrite(lightPins[i+1], HIGH);
-      if (sensoredDelay(1)) return;
-    }
-    digitalWrite(lightPins[6], LOW);
-    if (sensoredDelay(1)) return;
-    digitalWrite(lightPins[7], LOW);
-    if (sensoredDelay(1)) return;
-  } else if (program == 13) {
-    // right to left two lights
-    digitalWrite(lightPins[7], HIGH);
-    if (sensoredDelay(1)) return;
-    digitalWrite(lightPins[6], HIGH);
-    if (sensoredDelay(1)) return;
-    for (int i=6; i>0; i--) {
-      digitalWrite(lightPins[i+1], LOW);
-      digitalWrite(lightPins[i], HIGH);
-      digitalWrite(lightPins[i-1], HIGH);
-      if (sensoredDelay(1)) return;
-    }
-    digitalWrite(lightPins[1], LOW);
-    if (sensoredDelay(1)) return;
-    digitalWrite(lightPins[0], LOW);
-    if (sensoredDelay(1)) return;
-  } else if (program == 14) {
-    // kitt for remote
-    switchLights("01234567", HIGH);
-    if (sensoredDelay(10)) return;
-    switchLights("01234567", LOW);
-    if (sensoredDelay(15)) return;
-    for (int i=0; i<7; i++) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i+1], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(2)) return;
-    }
-    for (int i=7; i>0; i--) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i-1], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(2)) return;
-    }
-    digitalWrite(lightPins[0], LOW);
-    if (sensoredDelay(1200)) return;
   } else {
     // kitt
-    for (int i=0; i<7; i++) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i+1], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(2)) return;
+    for (byte i = 0; i < 7; i++) {
+      digitalWrite(lightPins[i], 1);
+      sensoredDelay(1);
+      digitalWrite(lightPins[i+1], 1);
+      sensoredDelay(1);
+      digitalWrite(lightPins[i], 0);
+      sensoredDelay(2);
     }
-    for (int i=7; i>0; i--) {
-      digitalWrite(lightPins[i], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i-1], HIGH);
-      if (sensoredDelay(1)) return;
-      digitalWrite(lightPins[i], LOW);
-      if (sensoredDelay(2)) return;
+    for (byte i = 7; i > 0; i--) {
+      digitalWrite(lightPins[i], 1);
+      sensoredDelay(1);
+      digitalWrite(lightPins[i-1], 1);
+      sensoredDelay(1);
+      digitalWrite(lightPins[i], 0);
+      sensoredDelay(2);
     }
   }
 }
 
-void switchLights(String leds, boolean value) {
-  for (int i=0; i<leds.length(); i++) {
-    int led = leds[i] - '0';
-    digitalWrite(lightPins[led], value);
+void flasher(byte firstFactor, byte secondFactor) {
+  switchLights(allLights, 8, 1);
+  sensoredDelay(firstFactor);
+  switchLights(allLights, 8, 0);
+  sensoredDelay(secondFactor);
+}
+
+void switchLights(byte lights[], byte count, byte value) {
+  for (byte i = 0; i < count; i++) {
+    digitalWrite(lightPins[lights[i]], value);
   }
 }
 
-boolean sensoredDelay(int factor) {
-  unsigned long startTime = millis();
-  unsigned long time = msMinPerProgram[program] * factor;
-  while (millis() - startTime < time) {
-    time = map(analogRead(speedPin), 1023, 0, msMinPerProgram[program], msMaxPerProgram[program]) * factor;
-    if (millis() - programSwitchTime > programSwitchDelay && digitalRead(programPin) == HIGH) {
-      program = program + 1;
-      if (program == (sizeof(msMinPerProgram)/sizeof(int))) {
-        program = 0;
-      }
-      switchLights("01234567", LOW);
-      programSwitchTime = millis();
+void sensoredDelay(byte factor) {
+  short poti = analogRead(A6);
+  short ms = poti / 12;
+  delay((ms + 10) * factor);
+}
 
-      return true;
+void programSwitch() {
+  if (millis() - programSwitchTime > 500) {
+    program++;
+    if (program > 10) {
+      program = 0;
     }
+    programSwitched = true;
+    programSwitchTime = millis();
   }
-  
-  return false;
 }
