d46786d18f23ed943ce181e489cbfc33d633685b
Michael Schindler <mi-schi@users.noreply.github.com>
2018-11-16 16:25:53 +0100

    Update program.ino

--- program.ino
+++ program.ino
@@ -1,4 +1,6 @@
-byte lightPins[8] = {5, 4, 3, 2, 1, 0, 9, 10};
+byte lightPins[8] = {0, 1, 2, 3, 4, 8, 9, 10};
+byte programPin = 8;
+byte speedPin = A7;
 
 byte allLights[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 byte halfLights[2][4] = {
@@ -14,24 +16,18 @@ byte splittedLights[4][4][2] = {
 };
 
 volatile byte program = 0;
-volatile bool programSwitched = false;
-volatile unsigned long programSwitchTime = 0;
+unsigned long programSwitchTime = 0;
 
 void setup() {
   for (byte i = 0; i < 8; i++) {
     pinMode(lightPins[i], OUTPUT);
   }  
-  pinMode(8, INPUT);
-  attachInterrupt(0, programSwitch, RISING);
+  pinMode(programPin, INPUT);
 
   flasher(20, 6);
 }
 
 void loop() {
-  if (programSwitched) {
-    switchLights(allLights, 8, 0);
-    programSwitched = false;
-  }
   if (program == 1 || program == 2) {
     // flasher / flasher 2 times
     flasher(6, (program == 2) ? 3 : 6);
@@ -65,14 +61,42 @@ void loop() {
   } else if (program >= 7 && program <= 10) {
     // splitted out to in snake / splitted left to right snake
     // splitted in to out snake / splitted right to left snake
-    for (byte value = 0; value < 2; value++) {
+    for (byte value = 1; value >= 0; value--) {
       for (byte i = 0; i < 4; i++) {
         switchLights(splittedLights[program - 7][i], 2, value);
         sensoredDelay(4);
       }
     }
-  } else {
-    // kitt
+  } else if (program >= 11 && program <= 14) {
+    // splitted out to in full, off / splitted left to right full, off
+    // splitted in to out full, off / splitted right to left full, off
+    for (byte i = 0; i < 4; i++) {
+      switchLights(splittedLights[program - 11][i], 2, 1);
+      sensoredDelay(4);
+    }
+    switchLights(allLights, 8, 0);
+    sensoredDelay(8);
+  } else if (program == 15) {
+    // snake
+    for (byte value = 1; value >= 0; value--) {
+      for (byte i = 0; i < 8; i++) {
+        digitalWrite(lightPins[i], value);
+        sensoredDelay(2);
+      }
+    } 
+  } else if (program == 16) {
+    // snake reverse
+    for (byte value = 1; value >= 0; value--) {
+      for (byte i = 7; i >= 0; i--) {
+        digitalWrite(lightPins[i], value);
+        sensoredDelay(2);
+      }
+    }
+  } else if (program == 0 || program == 17) {
+    // kitt // kitt with 2 min delay
+    if (program == 17) {
+      interruptedDelay(120000);
+    }
     for (byte i = 0; i < 7; i++) {
       digitalWrite(lightPins[i], 1);
       sensoredDelay(1);
@@ -106,18 +130,24 @@ void switchLights(byte lights[], byte count, byte value) {
 }
 
 void sensoredDelay(byte factor) {
-  short poti = analogRead(A6);
-  short ms = poti / 12;
-  delay((ms + 10) * factor);
+  unsigned long startTime = millis();
+  short poti = analogRead(speedPin);
+  short ms = (poti / 12 + 10) * factor;
+  interruptedDelay(ms);
 }
 
-void programSwitch() {
-  if (millis() - programSwitchTime > 500) {
-    program++;
-    if (program > 10) {
-      program = 0;
+void interruptedDelay(int ms) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < ms) {
+    if (millis() - programSwitchTime > 500 && digitalRead(programPin) == 1) {
+      program++;
+      if (program > 17) {
+        program = 0;
+      }
+      switchLights(allLights, 8, 0);
+      programSwitchTime = millis();
+
+      return;
     }
-    programSwitched = true;
-    programSwitchTime = millis();
   }
 }
